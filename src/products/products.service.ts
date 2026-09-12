93e7b94c259259f3ff9f73318a7e4098f685a59b
Rebeca <yoserebe00@gmail.com>
2026-09-12 13:19:56 -0600

    Update product DTO and update service method

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -30,6 +30,7 @@ export class ProductsService {
   ];
 
   create(createProductDto: CreateProductDto) {
+    if (!createProductDto.productId) createProductDto.productId = uuid()
     createProductDto.productId= uuid();
     this.products.push(createProductDto);
     return createProductDto;
@@ -53,14 +54,19 @@ export class ProductsService {
 
   update(id: string, updateProductDto: UpdateProductDto) {
     let product = this.findOne(id)
-    product = {
+    this.products = this.products.map((product) => {
+      if (product.productId === id) return {
+        ...product,
+        ...updateProductDto
+      }
+      return product;
+    })
+    return {
       ...product,
       ...updateProductDto,
     }
-    return product
- }
+  }
   
-
   remove(id: string) {
     const { productId } = this.findOne(id)
     this.products = this.products.filter((product) => product.productId !== productId)
